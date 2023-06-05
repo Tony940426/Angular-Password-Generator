1c81c19b5777d2f3e98e5a7383b27f5cc648c5ff
Tony <tony@DESKTOP-C5TKN10.localdomain>
2023-06-05 20:50:01 +1000

    Added properties for the checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
 
   onButtonClick(){
-    this.password = 'MY PASSWORD!!!'
+    this.password = 'MY PASSWORD!!!';
+    console.log(this.includeLetters, this.includeNumbers, this.includeSymbols);
   }
 
   onResetClick() {
     this.password = "";
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
   }
 }
